230901f5604aa30b4be8adca025c29e36c07f8b2
juan diego lopez <juandiegolopez1234@gmail.com>
2021-09-02 22:21:58 -0500

    01 mi primer commit en git para el programa

---
+++ my_program.cpp
@@ -0,0 +1,22 @@
+<stdio.h>
+<math.h>
+<fetch.h>
+
+#workspace 'curso devops'
+
+static int main [object ...]{
+
+int valor =0;
+int icontador=0;
+
+cin << 'Ingrese un valor para calcular una suma';
+
+for (i = 1; i <= ivalor; i++){
+	icontador+=i;
+}
+
+cout >> 'El resultado es: '+%integer,icontador;
+getch()
+ exit 1;
+
+}
