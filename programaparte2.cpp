c22bd08e742db760b0183b3c422a9065d1810621
juan diego lopez salamanca <juand.lopezs@campusucc.edu.co>
2021-10-21 20:32:34 -0500

    particionamiento de archivos

---
+++ programaparte2.cpp
@@ -0,0 +1,8 @@
+
+//Area funcion
+//vf_area calcular raiz cuadrada en una variable  e imprimir el resultado
+	raiz=sqrt(vf_area);
+	cout<<"la raiz cuadrada de" <<vf_area<< "es: "<<raiz;
+
+	return 0;
+}
