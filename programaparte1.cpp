ba229f25bb4712d691d6548321ffd2eb7afeaaaa
juan diego lopez salamanca <juand.lopezs@campusucc.edu.co>
2021-10-21 20:57:58 -0500

    se modifico cada programa

--- programaparte1.cpp
+++ programaparte1.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-
+         double vd_raiz=0;
 	int vi_opcion  = 1;
 	float vf_area  = 0;
 	bool vb_opcion = false;
@@ -79,13 +79,3 @@ int main() {
 	}
 
 	cout << "La area es => " << vf_area << endl; 
-	
-}
-
-//Area funcion
-//vf_area calcular raiz cuadrada en una variable  e imprimir el resultado
-	raiz=sqrt(vf_area);
-	cout<<"la raiz cuadrada de" <<vf_area<< "es: "<<raiz;
-
-	return 0;
-}
--- programaparte2.cpp
+++ programaparte2.cpp
@@ -1,8 +1,8 @@
 
-//Area funcion
-//vf_area calcular raiz cuadrada en una variable  e imprimir el resultado
-	raiz=sqrt(vf_area);
-	cout<<"la raiz cuadrada de" <<vf_area<< "es: "<<raiz;
-
+	//Area funcion
+	//vf_area calcular raiz cuadrada en una variable  e imprimir el resultado
+	vd_raiz=sqrt(vf_area);
+	cout<<"la raiz cuadrada de " << vf_area << " es: "<< vd_raiz;
+ 	cout<< "" ; 
 	return 0;
 }
