352c743238088a84060cf4ae25443f249b5efd28
juan diego lopez salamanca <juand.lopezs@campusucc.edu.co>
2021-10-21 20:42:43 -0500

    agregamos la particion al otro

--- programaparte1.cpp
+++ programaparte1.cpp
@@ -81,3 +81,11 @@ int main() {
 	cout << "La area es => " << vf_area << endl; 
 	
 }
+
+//Area funcion
+//vf_area calcular raiz cuadrada en una variable  e imprimir el resultado
+	raiz=sqrt(vf_area);
+	cout<<"la raiz cuadrada de" <<vf_area<< "es: "<<raiz;
+
+	return 0;
+}
