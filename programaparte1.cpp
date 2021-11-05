539628b49969184ccef74e4451b75791124be6a4
juan diego lopez salamanca <juand.lopezs@campusucc.edu.co>
2021-11-04 19:56:27 -0500

    programa hilo funcionando

--- programaparte1.cpp
+++ programaparte1.cpp
@@ -79,3 +79,11 @@ int main() {
 	}
 
 	cout << "La area es => " << vf_area << endl; 
+
+	//Area funcion
+	//vf_area calcular raiz cuadrada en una variable  e imprimir el resultado
+	vd_raiz=sqrt(vf_area);
+	cout<<"la raiz cuadrada de " << vf_area << " es: "<< vd_raiz;
+ 	cout<< "" ; 
+	return 0;
+}
